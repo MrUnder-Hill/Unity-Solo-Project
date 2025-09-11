4314ca201ba8be2520438619865af2222d250114
Joshua <joshuamichaelanderson11@gmail.com>
2025-09-11 10:25:49 -0700

    Player Camera System

--- Unity-Solo-Project/Assets/Scripts/Player Controller.cs
+++ Unity-Solo-Project/Assets/Scripts/Player Controller.cs
@@ -4,22 +4,47 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    Vector2 cameraRotation;
+    Vector3 cameraOffset;
+    InputAction lookVector;
+    Camera playerCam;
+
     Rigidbody rb;
 
     public float verticleMove;
     public float horizontalMove;
+
     public float speed = 5f;
     public float jumpHeight = 10f;
+    public float Xsensitivity = 1.0f;
+    public float Ysensitivity = 1.0f;
+    public float camRotationLimit = 90.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Start()
     {
+        cameraOffset = new Vector3(0, .5f, .5f);
         rb = GetComponent<Rigidbody>();
+        playerCam = Camera.main;
+        lookVector = GetComponent<PlayerInput>().currentActionMap.FindAction("Look");
+        cameraRotation = Vector2.zero;
     }
 
     // Update is called once per frame
     private void Update()
     {
+        // Camera Rotation System
+        playerCam.transform.position = transform.position + cameraOffset;
+
+        cameraRotation.x += lookVector.ReadValue<Vector2>().x * Xsensitivity;
+        cameraRotation.y += lookVector.ReadValue<Vector2>().y * Ysensitivity;
+
+        cameraRotation.y = Mathf.Clamp(cameraRotation.y, -camRotationLimit, camRotationLimit);
+
+        playerCam.transform.rotation = Quaternion.Euler(-cameraRotation.y, cameraRotation.x, 0);
+        transform.localRotation = Quaternion.AngleAxis(cameraRotation.x, Vector3.up);
+
+        // Movement System
         Vector3 temp = rb.linearVelocity;
 
         temp.x = verticleMove * speed;
